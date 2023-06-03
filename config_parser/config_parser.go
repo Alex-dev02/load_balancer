82c488625991fb69f60eed2590171089f3fb3bef
Alex-dev02 <ionut02alex@yahoo.ro>
2023-06-03 21:10:52 +0300

    config_parser struct and Init method

--- config_parser/config_parser.go
+++ config_parser/config_parser.go
@@ -1,4 +1,27 @@
 package configparser
 
+import (
+	"fmt"
+)
+
 type Config struct {
+	serverURLs                    []string
+	balancingAlgorithmName        string
+	serverTimeoutSeconds          int
+	failedHealthChecksTillTimeout int
+	slowStart                     bool
+	stickySession                 bool
+}
+
+func (this *Config) Init() {
+	this.serverURLs = make([]string, 0)
+	this.balancingAlgorithmName = "round-robin"
+	this.serverTimeoutSeconds = 30
+	this.failedHealthChecksTillTimeout = 3
+	this.slowStart = false
+	this.stickySession = false
+}
+
+func (this *Config) Print() {
+	fmt.Println(this)
 }
--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "github.com/Alex-dev02/load_balancer/load_balancer"
+import (
+	"github.com/Alex-dev02/load_balancer/load_balancer"
+	"github.com/Alex-dev02/load_balancer/config_parser"
+)
 
 func main() {
 	loadbalancer.Hello()
+	var config = new(configparser.Config)
+	config.Init()
+	config.Print()
 }
